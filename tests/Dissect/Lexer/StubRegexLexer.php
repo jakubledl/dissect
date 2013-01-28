00b6e40937542d109487b67e7ff5ad309f514e24
Jakub Lédl <jakubledl@gmail.com>
2013-01-28 18:34:13 +0100

    The regex lexer

---
+++ tests/Dissect/Lexer/StubRegexLexer.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Dissect\Lexer;
+
+class StubRegexLexer extends RegexLexer
+{
+    protected $operators = array('+', '-');
+
+    protected function getCatchablePatterns()
+    {
+        return array('[1-9][0-9]*');
+    }
+
+    protected function getNonCatchablePatterns()
+    {
+        return array('\s+');
+    }
+
+    protected function getType(&$value)
+    {
+        if (is_numeric($value)) {
+            $value = (int)$value;
+
+            return 'INT';
+        } elseif (in_array($value, $this->operators)) {
+            return $value;
+        } else {
+            throw new RuntimeException(sprintf('Invalid token "%s"', $value));
+        }
+    }
+}
